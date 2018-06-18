7f58069f7a23eee4890fb34f41920c07b24e1733
Erick-Lakkham <40186186+Erick-Lakkham@users.noreply.github.com>
2018-06-18 14:07:57 -0700

    New

--- http/JavaScript.js
+++ http/JavaScript.js
@@ -7,3 +7,33 @@ function factorsOf() {
 		}
 	}
 }
+function add() {
+	var a = prompt("First Number") * 1;
+	var b = prompt("Second Number") * 1;
+	document.getElementById("output").innerHTML = a + " + " + b + " = " + (a + b) + "<br/>" + document.getElementById("output").innerHTML;
+}
+function mul() {
+	var a = prompt("First Number") * 1;
+	var b = prompt("Second Number") * 1;
+	document.getElementById("output").innerHTML = a + " * " + b + " = " + (a * b) + "<br/>" + document.getElementById("output").innerHTML;
+}
+function exp() {
+	var a = prompt("Base") * 1;
+	var b = prompt("Exponent") * 1;
+	document.getElementById("output").innerHTML = a + " <sup> " + b + "</sup> = " + Math.pow(a, b) + "<br/>" + document.getElementById("output").innerHTML;
+}
+function sub() {
+	var a = prompt("First Number") * 1;
+	var b = prompt("Second Number") * 1;
+	document.getElementById("output").innerHTML = a + " - " + b + " = " + (a - b) + "<br/>" + document.getElementById("output").innerHTML;
+}
+function div() {
+	var a = prompt("First Number") * 1;
+	var b = prompt("Second Number") * 1;
+	document.getElementById("output").innerHTML = a + " / " + b + " = " + (a / b) + "<br/>" + document.getElementById("output").innerHTML;
+}
+function log() {
+	var a = prompt("Base") * 1;
+	var b = prompt("Number") * 1;
+	document.getElementById("output").innerHTML = "log<sub>" + a + "</sub>" + b + " = " + (Math.log(a) / Math.log(b)) + "<br/>" + document.getElementById("output").innerHTML;
+}
