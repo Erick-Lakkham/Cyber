121789440d961ed99166ffd8ba0f871e7caead1f
ebl <ebl@super.ksd.org>
2019-06-26 08:09:29 -0700

    Edhisive Assignment

---
+++ python/Edhesive Silly Sentences.py
@@ -0,0 +1,13 @@
+print ("Let's play Silly Sentences!\n")
+
+nam = input ("Enter a name: ")
+ad1 = input ("Enter an adjective: ")
+ad2 = input ("Enter an adjective: ")
+adv = input ("Enter an adverb: ")
+fd1 = input ("Enter a food: ")
+fd2 = input ("Enter another food: ")
+non = input ("Enter a noun: ")
+plc = input ("Enter a place: ")
+vrb = input ("Enter a verb: ")
+
+print ("\n" + nam + " was planning a dream vacation to " + plc + ".\n" + nam + " was especially looking forward to trying the local\ncuisine, including " + ad1 + " " + fd1 + " and " + fd2 + ".\n\n" + nam + " will have to practice the language " + adv + " to\nmake it easier to " + vrb + " with people.\n\n" + nam + " has a long list of sights to see, including the\n" + non + " museum and the " + ad2 + " park.")
